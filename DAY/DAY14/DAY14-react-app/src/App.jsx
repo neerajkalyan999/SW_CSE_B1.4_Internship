290172f3acda16c05a06aeb7f22ffd0dfecee340
neerajkalyan999 <neerajkalyan684@gmail.com>
2026-08-07 19:46:23 +0530

    List ex2: Unique Id

--- DAY/DAY14/DAY14-react-app/src/App.jsx
+++ DAY/DAY14/DAY14-react-app/src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
 import SimpleList from "./components/ListOne";
+import UserList from "./components/ListTwo";
+
+
 function App() {
   
   return (
     <>
-      <SimpleList />
+      
+      {/* <SimpleList /> */}
+      <UserList />
     </>
   )
 }
