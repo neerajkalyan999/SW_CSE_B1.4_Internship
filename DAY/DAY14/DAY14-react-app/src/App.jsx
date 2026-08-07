5d17a59cb3afa56cdb7394789e9ff9280ca01c02
neerajkalyan999 <neerajkalyan684@gmail.com>
2026-08-07 20:01:51 +0530

    Hooks ex3: useContext

--- DAY/DAY14/DAY14-react-app/src/App.jsx
+++ DAY/DAY14/DAY14-react-app/src/App.jsx
@@ -5,7 +5,7 @@ import UserList from "./components/ListTwo";
 import ProductListDemo from "./components/ListThree";
 import CounterHook from "./components/HookOne";
 import UserListHook from "./components/HookTwo";
-
+import ThemeProviderDemo from "./components/HookThree";
 
 
 function App() {
@@ -18,7 +18,8 @@ function App() {
       <ProductListDemo />
 
       <CounterHook/> */}
-      <UserListHook/>
+      {/* <UserListHook/> */}
+      <ThemeProviderDemo />
     </>
   )
 }
